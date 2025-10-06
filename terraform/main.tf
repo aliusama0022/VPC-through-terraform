b0d07fefecd266d2f761702d77967dda6d375673
aliusama0022 <aliusama.cloud@gmail.com>
2025-10-07 01:31:12 +0500

    Fix Terraform formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "october-7-2025"  
-    key            = "vpc/terraform.tfstate"
-    region         = "eu-north-1" 
-    encrypt        = true
+    bucket  = "october-7-2025"
+    key     = "vpc/terraform.tfstate"
+    region  = "eu-north-1"
+    encrypt = true
   }
 }
 
@@ -21,9 +21,9 @@ provider "aws" {
 }
 
 module "vpc" {
-  source              = "../modules/vpc"
-  vpc_cidr            = "10.0.0.0/16"
-  public_subnet_cidr  = "10.0.1.0/24"
-  availability_zone   = "eu-north-1a" 
-  vpc_name            = "modular-vpc"
+  source             = "../modules/vpc"
+  vpc_cidr           = "10.0.0.0/16"
+  public_subnet_cidr = "10.0.1.0/24"
+  availability_zone  = "eu-north-1a"
+  vpc_name           = "modular-vpc"
 }
